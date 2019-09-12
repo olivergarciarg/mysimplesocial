30819918fa43f8cb780cc5ba8bc8b404caca617e
GARCIA <OG961V@att.com>
2019-09-12 14:25:29 -0500

    login and signup views added

--- mysimplesocial/accounts/views.py
+++ mysimplesocial/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# below line deprecated
+# from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
+
 
 # Create your views here.
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
