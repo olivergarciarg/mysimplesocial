da6989290fbccc8194e754b3f8ac5129358d9319
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2019-10-01 09:54:06 -0500

    Update settings.py

--- mysimplesocial/mysimplesocial/settings.py
+++ mysimplesocial/mysimplesocial/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mm5(dk-vr)x08g2^zdgodwy179+%09!wsvb+c(w$ts#1a8=epd'
+SECRET_KEY = 'SUPER_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
